bfc5d323f7296a6f8c23118e66ab1f19c8f01954
Ignacio Lopez <ilopezp.chem@gmail.com>
2020-08-20 13:08:48 -0700

    Add project files.

---
+++ SrsDelayTestForm.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace StepperApp
+{
+    partial class SrsDelayTestApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonRefreshPorts = new System.Windows.Forms.Button();
+            this.buttonPort = new System.Windows.Forms.Button();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxGpibID = new System.Windows.Forms.ComboBox();
+            this.buttonTestSequence = new System.Windows.Forms.Button();
+            this.groupBoxStatus = new System.Windows.Forms.GroupBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.groupBoxSettings = new System.Windows.Forms.GroupBox();
+            this.groupBoxControl = new System.Windows.Forms.GroupBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBoxStatus.SuspendLayout();
+            this.groupBoxSettings.SuspendLayout();
+            this.groupBoxControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonRefreshPorts
+            // 
+            this.buttonRefreshPorts.BackColor = System.Drawing.Color.Transparent;
+            this.buttonRefreshPorts.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonRefreshPorts.Location = new System.Drawing.Point(83, 129);
+            this.buttonRefreshPorts.Name = "buttonRefreshPorts";
+            this.buttonRefreshPorts.Size = new System.Drawing.Size(80, 24);
+            this.buttonRefreshPorts.TabIndex = 0;
+            this.buttonRefreshPorts.TabStop = false;
+            this.buttonRefreshPorts.Text = "Refresh";
+            this.buttonRefreshPorts.UseVisualStyleBackColor = false;
+            this.buttonRefreshPorts.Click += new System.EventHandler(this.buttonRefreshPorts_Click);
+            // 
+            // buttonPort
+            // 
+            this.buttonPort.BackColor = System.Drawing.Color.Lime;
+            this.buttonPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPort.Location = new System.Drawing.Point(73, 93);
+            this.buttonPort.Name = "buttonPort";
+            this.buttonPort.Size = new System.Drawing.Size(100, 30);
+            this.buttonPort.TabIndex = 1;
+            this.buttonPort.Text = "Open Port";
+            this.buttonPort.UseVisualStyleBackColor = false;
+            this.buttonPort.Click += new System.EventHandler(this.buttonPort_Click);
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(73, 66);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxPorts.TabIndex = 0;
+            this.comboBoxPorts.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "COM Port :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "GPIB ID :";
+            // 
+            // comboBoxGpibID
+            // 
+            this.comboBoxGpibID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxGpibID.FormattingEnabled = true;
+            this.comboBoxGpibID.Location = new System.Drawing.Point(73, 39);
+            this.comboBoxGpibID.Name = "comboBoxGpibID";
+            this.comboBoxGpibID.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxGpibID.TabIndex = 2;
+            this.comboBoxGpibID.TabStop = false;
+            // 
+            // buttonTestSequence
+            // 
+            this.buttonTestSequence.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonTestSequence.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonTestSequence.Location = new System.Drawing.Point(255, 52);
+            this.buttonTestSequence.Name = "buttonTestSequence";
+            this.buttonTestSequence.Size = new System.Drawing.Size(100, 30);
+            this.buttonTestSequence.TabIndex = 3;
+            this.buttonTestSequence.TabStop = false;
+            this.buttonTestSequence.Text = "Test Sequence";
+            this.buttonTestSequence.UseVisualStyleBackColor = false;
+            this.buttonTestSequence.Click += new System.EventHandler(this.buttonTestSequence_Click);
+            // 
+            // groupBoxStatus
+            // 
+            this.groupBoxStatus.Controls.Add(this.textBoxStatus);
+            this.groupBoxStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxStatus.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxStatus.Name = "groupBoxStatus";
+            this.groupBoxStatus.Size = new System.Drawing.Size(173, 196);
+            this.groupBoxStatus.TabIndex = 28;
+            this.groupBoxStatus.TabStop = false;
+            this.groupBoxStatus.Text = "DEBUG";
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBoxStatus.Location = new System.Drawing.Point(3, 16);
+            this.textBoxStatus.Multiline = true;
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxStatus.Size = new System.Drawing.Size(167, 177);
+            this.textBoxStatus.TabIndex = 0;
+            // 
+            // groupBoxSettings
+            // 
+            this.groupBoxSettings.Controls.Add(this.comboBoxPorts);
+            this.groupBoxSettings.Controls.Add(this.buttonRefreshPorts);
+            this.groupBoxSettings.Controls.Add(this.buttonPort);
+            this.groupBoxSettings.Controls.Add(this.label2);
+            this.groupBoxSettings.Controls.Add(this.comboBoxGpibID);
+            this.groupBoxSettings.Controls.Add(this.label1);
+            this.groupBoxSettings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxSettings.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxSettings.Name = "groupBoxSettings";
+            this.groupBoxSettings.Size = new System.Drawing.Size(190, 196);
+            this.groupBoxSettings.TabIndex = 31;
+            this.groupBoxSettings.TabStop = false;
+            this.groupBoxSettings.Text = "Configuration";
+            // 
+            // groupBoxControl
+            // 
+            this.groupBoxControl.Controls.Add(this.buttonSend);
+            this.groupBoxControl.Controls.Add(this.buttonTestSequence);
+            this.groupBoxControl.Controls.Add(this.textBox1);
+            this.groupBoxControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxControl.Location = new System.Drawing.Point(0, 0);
+            this.groupBoxControl.Name = "groupBoxControl";
+            this.groupBoxControl.Size = new System.Drawing.Size(367, 88);
+            this.groupBoxControl.TabIndex = 30;
+            this.groupBoxControl.TabStop = false;
+            this.groupBoxControl.Text = "Control";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.BackColor = System.Drawing.SystemColors.Control;
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSend.Location = new System.Drawing.Point(255, 16);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(100, 30);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = false;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(206, 20);
+            this.textBox1.TabIndex = 30;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBoxControl);
+            this.splitContainer1.Size = new System.Drawing.Size(367, 288);
+            this.splitContainer1.SplitterDistance = 196;
+            this.splitContainer1.TabIndex = 32;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBoxSettings);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.groupBoxStatus);
+            this.splitContainer2.Size = new System.Drawing.Size(367, 196);
+            this.splitContainer2.SplitterDistance = 190;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // SrsDelayTestApp
+            // 
+            this.AcceptButton = this.buttonSend;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(367, 288);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(600, 1000);
+            this.Name = "SrsDelayTestApp";
+            this.Text = "SrsDelayTestApp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.SrsDelayTestApp_FormClosing);
+            this.Load += new System.EventHandler(this.SrsDelayTestApp_Load);
+            this.groupBoxStatus.ResumeLayout(false);
+            this.groupBoxStatus.PerformLayout();
+            this.groupBoxSettings.ResumeLayout(false);
+            this.groupBoxSettings.PerformLayout();
+            this.groupBoxControl.ResumeLayout(false);
+            this.groupBoxControl.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+        private System.Windows.Forms.Button buttonPort;
+        private System.Windows.Forms.Button buttonRefreshPorts;
+        private System.Windows.Forms.GroupBox groupBoxStatus;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Button buttonTestSequence;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxGpibID;
+        private System.Windows.Forms.GroupBox groupBoxSettings;
+        private System.Windows.Forms.GroupBox groupBoxControl;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ SrsDelayTestForm.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// this app reads units (degree), and requests an arduino to move a motor some # of steps.
+// requires polarizerControl software to be installed on an arduino
+//
+namespace StepperApp
+{
+    public partial class SrsDelayTestApp : Form
+    {
+        #region Prologix Constants 
+        private const string PrologixAddress = "++addr";
+        private const string PrologixIFC = "++ifc";
+        private const string PrologixMode = "++mode 1";
+        private const string PrologixEOI = "++eoi 1";
+        private const string PrologixEOS = "++eos 0"; // 0 = CRLF termination
+        private const string PrologixRead = "++read";
+        #endregion
+
+        private int Timeout { get; set; } = 500;
+
+        public SrsDelayTestApp()
+        {
+            InitializeComponent();
+        }
+
+        // initializes the form
+        private void SrsDelayTestApp_Load(object sender, EventArgs e)
+        {
+            getPorts();
+
+            groupBoxControl.Enabled = false;
+            foreach (var address in Enumerable.Range(0, 31)) comboBoxGpibID.Items.Add(address);
+            comboBoxGpibID.SelectedItem = 15;
+            buttonPort.Focus();
+        }
+
+        #region Com Port Controls
+
+        // populates com ports
+        private void getPorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBoxPorts.Items.Add(port);
+            }
+        }
+        // carefully close port 
+        private void ClosePort()
+        {
+
+            // update panels
+            textBoxStatus.AppendText("Disconnected");
+
+            buttonPort.Text = "Open Port";
+            buttonPort.BackColor = Color.Lime;
+            buttonPort.Focus();
+
+            comboBoxPorts.Enabled = true;
+            buttonRefreshPorts.Enabled = true;
+            groupBoxControl.Enabled = false;
+
+            serialPort1.Close();
+            serialPort1.Dispose();
+        }
+
+        // refreshes available ports
+        private void buttonRefreshPorts_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                comboBoxPorts.Items.Clear();
+                getPorts();
+            }
+        }
+        #endregion
+
+        // This opens the serial port if it is available
+        private void buttonPort_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false && comboBoxPorts.SelectedIndex != -1)
+            {
+                #region Serial Port Config
+                serialPort1.PortName = comboBoxPorts.Text;
+                serialPort1.BaudRate = 9600; // baud rate does not matter
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Handshake = Handshake.RequestToSend;
+                serialPort1.RtsEnable = true;
+                serialPort1.DtrEnable = true;
+                serialPort1.Encoding = Encoding.ASCII;
+                // for error handling 
+                serialPort1.DiscardNull = false;
+                serialPort1.ParityReplace = 0;
+                //Timeout settings
+                serialPort1.ReadTimeout = Timeout;
+                serialPort1.WriteTimeout = Timeout;
+                #endregion
+
+                try
+                {
+                    serialPort1.Open();
+                    serialPort1.DiscardOutBuffer();
+                    serialPort1.DiscardInBuffer();
+
+                    buttonPort.Text = "Close Port";
+                    buttonPort.BackColor = SystemColors.Control;
+
+                    comboBoxPorts.Enabled = false;
+                    buttonRefreshPorts.Enabled = false;
+                    groupBoxControl.Enabled = true;
+                    buttonSend.Focus();
+
+                    // set up prologix controller
+                    SendTX(PrologixIFC); // Make Prologix Controller-In-Charge
+                    SendTX(PrologixMode); // Use CONTROLLER mode
+                    SendTX(PrologixEOI); // Assert EOI after transmit GPIB data.
+                    SendTX(PrologixEOS); // Use CRLF as GPIB terminator.
+                    SendTX("++auto 1"); // Enable read after write.
+                    SendTX("++read_tmo_ms " + Timeout); // Set readout for manual 
+                    SendTX("++ver");
+                }
+                catch
+                {
+                    textBoxStatus.AppendText("Prologix: Device Not Available");
+                }
+            }
+            else
+            {
+                try
+                {
+                    ClosePort();
+                }
+                catch (IOException ex)
+                {
+                    textBoxStatus.AppendText(ex.Message);
+                }
+            }
+        }
+
+        private delegate void InvokeDelegate(string s);
+        private async void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string RXbuffer = serialPort1.ReadExisting();
+            {
+                this.BeginInvoke(new InvokeDelegate(UpdateStatus), new object[] { RXbuffer });
+            }
+            await Task.Delay(100);
+        }
+
+        private void UpdateStatus(string RXbuffer)
+        {
+            textBoxStatus.AppendText(RXbuffer);
+        }
+
+        /// <summary>
+        /// This function sends serial commands to the prologix controller.
+        /// If commands begin with ++ they are sent to the controller.
+        /// Otherwise, commands are relayed to the gpib instrument.
+        /// </summary>
+        /// <param name="command"></param>
+        private void SendTX(string command)
+        {
+            string TX;
+            if (command[0] == (char)43) // send to prologix 
+            {
+                TX = command + "\r\n";
+            }
+            else
+            {
+                TX = EscapeString(command) + "\r\n"; // send to instrument
+            }
+
+            textBoxStatus.AppendText("> " + command + "\r\n");
+            try
+            {
+                if (!serialPort1.IsOpen) serialPort1.Open();
+                serialPort1.Write(TX);
+            }
+            catch (IOException ex)
+            {
+                textBoxStatus.AppendText(ex.Message);
+                ClosePort();
+            }
+        }
+
+        /// <summary>
+        /// This function escapes the GPIB command string with ascii null characters for use with Prologix controller.
+        /// CR (13), LF (10), ESC (27), + (43) characters will also be escaped.
+        /// </summary>
+        /// <param name="s">GPIB command</param>
+        /// <returns>Null-escaped string</returns>
+        string EscapeString(string s)
+        {
+            StringBuilder builder = new StringBuilder(s.Length);
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == (char)10 || s[i] == (char)13 || s[i] == (char)27 || s[i] == (char)43)
+                {
+                    builder.Append((char)27); // escape
+                }
+                builder.Append(s[i]);
+                builder.Append('\0'); // Hack/workaround for every-other-character problem.
+            }
+            return builder.ToString();
+        }
+
+        #region Background workers 
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (!serialPort1.IsOpen) serialPort1.Open();
+            serialPort1.Write("++auto 0\r\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                double Delay;
+                if (i % 2 == 0)
+                {
+                    Delay = 1.5E-5;
+                }
+                else
+                {
+                    Delay = 2.5E-3;
+                }
+
+                serialPort1.DiscardInBuffer();
+                serialPort1.DiscardOutBuffer();
+
+                string WriteDelayCommand = "DT 2,1," + Delay.ToString();
+                serialPort1.Write(EscapeString(WriteDelayCommand) + "\r\n"); //"\r\n" ???
+
+                string ReadDelayCommand = "DT 2";
+                serialPort1.Write(EscapeString(ReadDelayCommand) + "\r\n");
+                serialPort1.Write("++read\r\n");
+                Thread.Sleep(100);
+
+                backgroundWorker1.ReportProgress(0, serialPort1.ReadExisting());
+            }
+            serialPort1.Write("++auto 1\r\n");
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            textBoxStatus.AppendText(e.UserState as string);
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            groupBoxControl.Enabled = true;
+        }
+
+        #endregion 
+
+        private void buttonTestSequence_Click(object sender, EventArgs e)
+        {
+            groupBoxControl.Enabled = false;
+            //BackgroundWorker backgroundWorker1 = new BackgroundWorker();
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            backgroundWorker1.RunWorkerCompleted += backgroundWorker1_RunWorkerCompleted;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string sendText = textBox1.Text;
+            if (string.IsNullOrEmpty(sendText) == false)
+            {
+                SendTX(sendText); // Raw Data send//"\r\n" ???
+            }
+        }
+
+        // quit application
+        private void SrsDelayTestApp_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                ClosePort();
+            }
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
